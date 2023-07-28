6424441f41d2de0020841c43035632a3ae0c1556
BatoAlvin <alvinbato112@gmail.com>
2023-07-28 17:15:42 +0300

    edited logo

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -47,7 +47,7 @@ export const Skills = () => {
                             </div>
                             <div className="item">
                                 <img src={meter3} alt="Image" />
-                                <h5>Logo Design</h5>
+                                <h5>Logo Designs</h5>
                             </div>
                             <div className="item">
                                 <img src={meter1} alt="Image" />
