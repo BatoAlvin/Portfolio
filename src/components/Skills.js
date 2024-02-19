a3320300891657904f9293a06072257c7ce07151
BatoAlvin <alvinbato112@gmail.com>
2024-02-19 14:02:13 +0300

    new skill

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -47,7 +47,7 @@ export const Skills = () => {
                             </div>
                             <div className="item">
                                 <img src={meter3} alt="Image" />
-                                <h5>Team Player</h5>
+                                <h5>Mobile Development</h5>
                             </div>
                             <div className="item">
                                 <img src={meter1} alt="Image" />
