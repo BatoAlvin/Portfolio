d089abb04ac841e19e432c07095a4b2369fbe7c1
BatoAlvin <alvinbato112@gmail.com>
2024-02-19 14:37:55 +0300

    heading edit

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,7 +11,7 @@ export const Banner = () => {
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  const toRotate = [ "System Developer", "Website Designer" ];
+  const toRotate = [ "System Developer", "Mobile Developer","Website Designer" ];
   const period = 2000;
 
   useEffect(() => {
