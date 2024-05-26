a406bd7b1e52208556711a2ec98f785a6876fcbd
Maria Angella <maria5angella@gmail.com>
2024-05-26 13:48:45 +0300

    card modification

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,13 +1,38 @@
 import { Col } from "react-bootstrap";
 
-export const ProjectCard = ({ title, description, imgUrl }) => {
+export const ProjectCard = ({ title, description, imgUrl,url,url2,paragraph }) => {
+  const myStyle = {
+    color: 'white',
+    textDecoration: 'none'
+  };
+
   return (
     <Col size={12} sm={6} md={4}>
       <div className="proj-imgbx">
         <img src={imgUrl} />
         <div className="proj-txtx">
-          <h4>{title}</h4>
-          <span>{description}</span>
+          <h4>
+          {url ? (
+              <a href={url} target="_blank" rel="noopener noreferrer" style={{ color: 'white', textDecoration: 'none' }}>
+                {title}
+              </a>
+            ) : (
+              title
+            )}
+          </h4>
+          <span>{description}</span><br></br>
+
+          <p style={myStyle}>
+          {url2 ? (
+              <a href={url2} target="_blank" rel="noopener noreferrer" style={{ color: 'white', textDecoration: 'none' }}>
+                {paragraph}
+              </a>
+            ) : (
+              paragraph
+            )}
+            
+            
+            </p>
         </div>
       </div>
     </Col>
